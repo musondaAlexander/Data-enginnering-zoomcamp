140109758fbb3aa5fa55b85bd739fb980f312977
Alexander Musonda <musondaalexander97@gmail.com>
2025-01-26 10:19:47 +0200

    Fix typo in pipeline.py comments and improve output message

--- week_one/DOCKER_SQL/pipeline.py
+++ week_one/DOCKER_SQL/pipeline.py
@@ -3,6 +3,6 @@ import sys
 
 #  we wil user this file to create a pipeline
 print(sys.argv)
-#  we will use this file to create a pipeline
+
 day = sys.argv[1]
 print("Hello job successfull runned for day", day)
