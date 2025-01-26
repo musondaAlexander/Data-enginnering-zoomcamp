523e657683c3612d56d4273ec82167e6c42ff548
Alexander Musonda <musondaalexander97@gmail.com>
2025-01-26 10:16:17 +0200

    Add initial project structure with PostgreSQL setup and data pipeline

---
+++ week_one/DOCKER_SQL/pipeline.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import sys
+
+#  we wil user this file to create a pipeline
+print(sys.argv)
+#  we will use this file to create a pipeline
+day = sys.argv[1]
+print("Hello job successfull runned for day", day)
